6f7d88876f610cdce6a6c0d87cd8d2a7b53a406d
Nathaniel Callahan <ntctk4@mail.missouri.edu>
2020-09-30 22:01:42 -0500

    Add files via upload
    Added converter app to repo

---
+++ Ntctk4ConverterApp/Converter.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ntctk4ConverterApp
+{
+    static class Converter
+    {
+        public static Double toInches(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Inches"):
+                    answer = startValue;
+                    break;
+                case ("Feet"):
+                    answer = startValue * 12;
+                    break;
+                case ("Yards"):
+                    answer = startValue * 36;
+                    break;
+                case ("Miles"):
+                    answer = startValue * 63360;
+                    break;
+                case ("Centimeters"):
+                    answer = startValue * 0.393701;
+                    break;
+                case ("Meters"):
+                    answer = startValue * 39.3701;
+                    break;
+                case ("Kilometers"):
+                    answer = startValue * 39370.1;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toFeet(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Inches"):
+                    answer = startValue / 12;
+                    break;
+                case ("Feet"):
+                    answer = startValue;
+                    break;
+                case ("Yards"):
+                    answer = startValue * 3;
+                    break;
+                case ("Miles"):
+                    answer = startValue * 5280;
+                    break;
+                case ("Centimeters"):
+                    answer = startValue * 0.0328084;
+                    break;
+                case ("Meters"):
+                    answer = startValue * 3.28084;
+                    break;
+                case ("Kilometers"):
+                    answer = startValue * 3280.84;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toYards(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Inches"):
+                    answer = startValue * 0.0277778;
+                    break;
+                case ("Feet"):
+                    answer = startValue * 0.333333;
+                    break;
+                case ("Yards"):
+                    answer = startValue;
+                    break;
+                case ("Miles"):
+                    answer = startValue * 1760;
+                    break;
+                case ("Centimeters"):
+                    answer = startValue * 0.0109361;
+                    break;
+                case ("Meters"):
+                    answer = startValue * 1.09361;
+                    break;
+                case ("Kilometers"):
+                    answer = startValue * 1093.61;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toMiles(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Inches"):
+                    answer = startValue * 1.57826e-5;
+                    break;
+                case ("Feet"):
+                    answer = startValue * 0.000189394;
+                    break;
+                case ("Yards"):
+                    answer = startValue * 0.000568182;
+                    break;
+                case ("Miles"):
+                    answer = startValue;
+                    break;
+                case ("Centimeters"):
+                    answer = startValue * 6.21371e-6;
+                    break;
+                case ("Meters"):
+                    answer = startValue * 0.000621371;
+                    break;
+                case ("Kilometers"):
+                    answer = startValue * 0.621371;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toCentimeters(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Inches"):
+                    answer = startValue * 2.54;
+                    break;
+                case ("Feet"):
+                    answer = startValue * 30.48;
+                    break;
+                case ("Yards"):
+                    answer = startValue * 91.44;
+                    break;
+                case ("Miles"):
+                    answer = startValue * 160934;
+                    break;
+                case ("Centimeters"):
+                    answer = startValue;
+                    break;
+                case ("Meters"):
+                    answer = startValue * 100;
+                    break;
+                case ("Kilometers"):
+                    answer = startValue * 100000;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toMeters(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Inches"):
+                    answer = startValue * 0.0254;
+                    break;
+                case ("Feet"):
+                    answer = startValue * 0.3048;
+                    break;
+                case ("Yards"):
+                    answer = startValue * 0.9144;
+                    break;
+                case ("Miles"):
+                    answer = startValue * 1609.34;
+                    break;
+                case ("Centimeters"):
+                    answer = startValue * 0.01;
+                    break;
+                case ("Meters"):
+                    answer = startValue;
+                    break;
+                case ("Kilometers"):
+                    answer = startValue * 1000;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toKilometers(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Inches"):
+                    answer = startValue * 2.54e-5;
+                    break;
+                case ("Feet"):
+                    answer = startValue * 0.0003048;
+                    break;
+                case ("Yards"):
+                    answer = startValue * 0.0009144;
+                    break;
+                case ("Miles"):
+                    answer = startValue * 1.60934;
+                    break;
+                case ("Centimeters"):
+                    answer = startValue * 1e-5;
+                    break;
+                case ("Meters"):
+                    answer = startValue * 0.001;
+                    break;
+                case ("Kilometers"):
+                    answer = startValue;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toFahrenheit(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Fahrenheit"):
+                    answer = startValue;
+                    break;
+                case ("Celsius"):
+                    answer = (startValue * 1.8)+32;
+                    break;
+                case ("Kelvin"):
+                    answer = (startValue * 1.8) - 459.67;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toCelsius(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Fahrenheit"):
+                    answer = (startValue - 32) * 1.8;
+                    break;
+                case ("Celsius"):
+                    answer = startValue;
+                    break;
+                case ("Kelvin"):
+                    answer = startValue - 273.15;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toKelvin(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Fahrenheit"):
+                    answer = (startValue + 459.67) * 1.8;
+                    break;
+                case ("Celsius"):
+                    answer = startValue + 273.15;
+                    break;
+                case ("Kelvin"):
+                    answer = startValue;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toPounds(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Pounds"):
+                    answer = startValue;
+                    break;
+                case ("Grams"):
+                    answer = startValue * 0.00220462;
+                    break;
+                case ("Kilograms"):
+                    answer = startValue * 2.20462;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toGrams(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Pounds"):
+                    answer = startValue * 453.592;
+                    break;
+                case ("Grams"):
+                    answer = startValue;
+                    break;
+                case ("Kilograms"):
+                    answer = startValue * 1000;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+        public static Double toKilograms(Double startValue, String units)
+        {
+            Double answer = 0;
+            switch (units)
+            {
+                case ("Pounds"):
+                    answer = startValue * 0.453592;
+                    break;
+                case ("Grams"):
+                    answer = startValue * 0.001;
+                    break;
+                case ("Kilograms"):
+                    answer = startValue;
+                    break;
+                default: break;
+            }
+
+            return answer;
+        }
+
+    }
+}
---
+++ Ntctk4ConverterApp/MainWindow.xaml.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ntctk4ConverterApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Console.Write(comboBox.SelectedItem.ToString());
+            //comboBox1.Items.Clear();
+            //comboBox1.Items.Add("Cori");
+            ComboBoxItem item = (ComboBoxItem)comboBox.SelectedItem;
+            String value = item.Content.ToString();
+            if (value.Equals("Inches") ||
+                value.Equals("Feet") ||
+                value.Equals("Yards") ||
+                value.Equals("Miles") ||
+                value.Equals("Centimeters") ||
+                value.Equals("Meters") ||
+                value.Equals("Kilometers"))
+            {
+                if (comboBox1.Items.Count <= 0 || !comboBox1.Items[0].ToString().Equals("Inches"))
+                {
+                    comboBox1.Items.Clear();
+                    comboBox1.Items.Add("Inches");
+                    comboBox1.Items.Add("Feet");
+                    comboBox1.Items.Add("Yards");
+                    comboBox1.Items.Add("Miles");
+                    comboBox1.Items.Add("Centimeters");
+                    comboBox1.Items.Add("Meters");
+                    comboBox1.Items.Add("Kilometers");
+                }
+            }
+            else if (value.Equals("Fahrenheit") ||
+                value.Equals("Celsius") ||
+                value.Equals("Kelvin"))
+            {
+                if (comboBox1.Items.Count <= 0 || !comboBox1.Items[0].ToString().Equals("Fahrenheit"))
+                {
+                    comboBox1.Items.Clear();
+                    comboBox1.Items.Add("Fahrenheit");
+                    comboBox1.Items.Add("Celsius");
+                    comboBox1.Items.Add("Kelvin");
+                }
+            }
+            else if(value.Equals("Pounds") ||
+                value.Equals("Grams") ||
+                value.Equals("Kilograms"))
+            {
+                if (comboBox1.Items.Count <= 0 || !comboBox1.Items[0].ToString().Equals("Pounds"))
+                {
+                    comboBox1.Items.Clear();
+                    comboBox1.Items.Add("Pounds");
+                    comboBox1.Items.Add("Grams");
+                    comboBox1.Items.Add("Kilograms");
+                }
+            }
+
+        }
+
+        //not sure I need this event since I don't change anything if the user 
+        //closes the drop box without changing anything
+        private void comboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            Console.Write(comboBox.SelectedItem);
+        }
+
+        private void convert_Click(object sender, RoutedEventArgs e)
+        {
+            Double startValue = 0;
+            Double convertedValue = 0;
+            Boolean success = true;
+            if (!Double.TryParse(startingNumTextBox.Text, out startValue))
+            {
+                solutionTextBlock.Text = "Invalid starting value! Please reenter a numerical value to convert.";
+                return;
+            }
+
+            String value1 = "";
+            String value2 = "";
+            try
+            {
+                ComboBoxItem item = (ComboBoxItem)comboBox.SelectedItem;
+                value1 = item.Content.ToString();
+                value2 = comboBox1.SelectedItem.ToString();
+            }
+            catch(NullReferenceException ex)
+            {
+                solutionTextBlock.Text = "Please select a starting unit and ending unit from the drop down boxes and try again!";
+                Console.Write(ex.Message);
+                return;
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+            switch (value2)
+            {
+                case ("Inches"):
+                    if (startValue < 0)
+                    {
+                        solutionTextBlock.Text = "Distance can't be negitive, try again.";
+                        success = false;
+                    }
+                    else
+                        convertedValue = Converter.toInches(startValue, value1);
+                    break;
+                case ("Feet"):
+                    if (startValue < 0)
+                    {
+                        solutionTextBlock.Text = "Distance can't be negitive, try again.";
+                        success = false;
+                    }
+                    else
+                        convertedValue = Converter.toFeet(startValue, value1);
+                    break;
+                case ("Yards"):
+                    if (startValue < 0)
+                    {
+                        solutionTextBlock.Text = "Distance can't be negitive, try again.";
+                        success = false;
+                    }
+                    else
+                        convertedValue = Converter.toYards(startValue, value1);
+                    break;
+                case ("Miles"):
+                    if (startValue < 0)
+                    {
+                        solutionTextBlock.Text = "Distance can't be negitive, try again.";
+                        success = false;
+                    }
+                    else
+                        convertedValue = Converter.toMiles(startValue, value1);
+                    break;
+                case ("Centimeters"):
+                    if (startValue < 0)
+                    {
+                        solutionTextBlock.Text = "Distance can't be negitive, try again.";
+                        success = false;
+                    }
+                    else
+                        convertedValue = Converter.toCentimeters(startValue, value1);
+                    break;
+                case ("Meters"):
+                    if (startValue < 0)
+                    {
+                        solutionTextBlock.Text = "Distance can't be negitive, try again.";
+                        success = false;
+                    }
+                    else
+                        convertedValue = Converter.toMeters(startValue, value1);
+                    break;
+                case ("Kilometers"):
+                    if (startValue < 0)
+                    {
+                        solutionTextBlock.Text = "Distance can't be negitive, try again.";
+                        success = false;
+                    }
+                    else
+                        convertedValue = Converter.toKilometers(startValue, value1);
+                    break;
+                case ("Fahrenheit"):
+                    convertedValue = Converter.toFahrenheit(startValue, value1);
+                    break;
+                case ("Celsius"):
+                    convertedValue = Converter.toCelsius(startValue, value1);
+                    break;
+                case ("Kelvin"):
+                    convertedValue = Converter.toKelvin(startValue, value1);
+                    break;
+                case ("Pounds"):
+                    if (startValue < 0)
+                    {
+                        solutionTextBlock.Text = "Mass can't be negitive, try again.";
+                        success = false;
+                    }
+                    else
+                        convertedValue = Converter.toPounds(startValue, value1);
+                    break;
+                case ("Grams"):
+                    if (startValue < 0)
+                    {
+                        solutionTextBlock.Text = "Mass can't be negitive, try again.";
+                        success = false;
+                    }
+                    else
+                        convertedValue = Converter.toGrams(startValue, value1);
+                    break;
+                case ("Kilograms"):
+                    if (startValue < 0)
+                    {
+                        solutionTextBlock.Text = "Mass can't be negitive, try again.";
+                        success = false;
+                    }
+                    else
+                        convertedValue = Converter.toKilograms(startValue, value1);
+                    break;
+                default: break;
+            }
+            if(success.Equals(true))
+                solutionTextBlock.Text = convertedValue.ToString();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a measurement converter made for Infotec 4400 (C#) final project \nby: Nathaniel Callahan");
+        }
+    }
+}
